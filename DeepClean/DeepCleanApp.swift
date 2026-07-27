6f6a638756dddcd59faee1de9c98b542b155e068
Leibniz Li <stormte@gmail.com>
2026-07-27 20:52:49 +0800

    Initial Commit

---
+++ DeepClean/DeepCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepCleanApp.swift
+//  DeepClean
+//
+//  Created by admin on 2026/7/27.
+//
+
+import SwiftUI
+
+@main
+struct DeepCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
